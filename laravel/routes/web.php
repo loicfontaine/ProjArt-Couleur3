8a36c618ea50985e7ac75f1b02e66ea6e177dd6d
LeaTalon <79629352+kassandrawoods@users.noreply.github.com>
2023-05-30 10:34:57 +0200

    env

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/challenge', function () {
+    return view('challenge');
 });
