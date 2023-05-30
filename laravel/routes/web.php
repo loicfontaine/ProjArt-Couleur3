af3126e07a7201a7dd21a89c7035b2d7398a12d0
Nisa Azizi <nisa.azizi@heig-vd.ch>
2023-05-30 15:35:37 +0200

    started home page

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -18,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/connexion', function () {
     return view('connexion');
 });
+
+Route::get('/', function () {
+    return view('homePage');
+});
